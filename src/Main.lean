3abc3c0d6aec23f94e22a683336fcd8717b02998
James Parker <james@galois.com>
2025-01-10 10:09:02 -0500

    Initial API prototype

--- src/Main.lean
+++ src/Main.lean
@@ -1,4 +1,57 @@
+import Mathlib.Algebra.Field.Defs
 import ZkLean
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println s!"Hello!"
+
+
+
+
+
+
+def example1 [Field f] : ZKBuilder (ZKVar f) := do
+  let x: ZKVar f <- witness
+  let one: ZKVar f := 1
+  constrain (x * (x - one) === 0)
+  return x
+
+
+
+
+def eq32 [Field f] : LookupTable f := {}
+
+
+
+structure RISCVState (f: Type) where
+  pc: ZKVar f
+  registers: Vector (ZKVar f) 32
+
+def example2 [Field f] (prev_st : RISCVState f) : ZKBuilder (RISCVState f) := do
+  let new_st <- witness
+
+  let r1 := prev_st.registers[1]
+  let r2 := prev_st.registers[2]
+
+  let isEq <- lookup eq32 r1 r2
+  constrain (new_st.registers[0] === isEq)
+
+  pure new_st
+
+-- structure RISCVState (backend : Type) where
+--   pc: ZKRepr backend Unit
+--   -- registers: List (zkrepr UInt32)
+
+-- #check RISCVState.mk 32
+
+-- structure [ZKRepr zkrepr] RISCVState (zkrepr : Type) where
+--   pc: repr zkrepr UInt32
+--   -- registers: List (zkrepr UInt32)
+
+-- def example2 {zkrepr:Type} [ZKRepr1 zkrepr Unit Unit] : ZKBuilder (RISCVState (ZKRepr1 zkrepr Unit)) := do
+-- def example2 {zkrepr:Type} : ZKBuilder (RISCVState zkrepr) := do
+--   let new_st <- witness
+-- 
+--   pure new_st
+  
+
+-- #eval example1
--- src/ZkLean/Basic.lean
+++ src/ZkLean/Basic.lean
@@ -1,1 +1,76 @@
-def hello := "world"
+
+structure ZKVar (a: Type) where
+  -- TODO: identifier
+
+instance [OfNat a n] : OfNat (ZKVar a) n where
+  ofNat := {}
+
+instance [HSub a a a] : HSub (ZKVar a) (ZKVar a) (ZKVar a) where
+  hSub _x _y := {}
+
+instance [HMul a a a] : HMul (ZKVar a) (ZKVar a) (ZKVar a) where
+  hMul _x _y := {}
+
+-- TODO: Free monad?
+structure ZKBuilder (a: Type) where
+  -- TODO: environment? AST?
+
+-- ZKRepr ??
+-- ZKRepr Jolt u32 = F128p
+
+instance : Monad ZKBuilder where
+  pure _x := {} -- TODO
+  bind _opt _next := {} -- TODO
+
+-- def witness {t : Type} : ZKBuilder (ZKVar t) := 
+def witness {t : Type} : ZKBuilder t := 
+  {} -- TODO
+
+def constrain (_constraint: ZKBuilder Bool) : ZKBuilder Unit := {}
+def constrainEq (_x: ZKVar a) (_y: ZKVar a) : ZKBuilder Bool := {}
+infix:50    " === " => constrainEq
+
+
+structure LookupTable (f: Type) where
+
+def lookup (_table : LookupTable f) (_:ZKVar f) (_:ZKVar f) : ZKBuilder (ZKVar f) := {}
+
+
+
+
+
+
+
+-- TODO: Debug this type family
+
+class ZKBackend (a: Type) where
+  ZKRepr : Type -> Type
+
+instance :ZKBackend Unit where
+  -- ZKRepr : Type -> Type
+  ZKRepr Unit := UInt32
+  -- | Unit => UInt32
+
+#check ZKBackend Unit
+#check ZKBackend.ZKRepr Unit Unit
+-- #eval (32: ZKBackend.ZKRepr Unit Unit)
+
+-- def ZKRepr := Type -> Type -> Type
+-- def ZKRepr : Type -> Type -> Type
+-- abbrev ZKRepr : Type -> Type
+-- | Unit => UInt32
+
+
+-- class ZKRepr (a: Type) where
+--   repr : Type -> Type
+--   -- repr (t: Type) : Type
+
+-- -- type F128p := Nat
+-- 
+-- class ZKRepr1 (s: Type) (t: Type) (u: Type) where
+-- 
+-- 
+-- inductive Jolt where
+-- instance : ZKRepr1 Jolt UInt32 (ZKVar F128p) where
+
+
