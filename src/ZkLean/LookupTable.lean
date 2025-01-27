993550041dcf807fd43854103d08a9c906eed0c0
James Parker <james@galois.com>
2025-01-27 12:10:20 -0500

    Sketching

--- src/ZkLean/LookupTable.lean
+++ src/ZkLean/LookupTable.lean
@@ -3,6 +3,13 @@ import Mathlib.Algebra.Field.Defs
 
 inductive LookupTable (f: Type) where
   | LookupTableMLE (n: Nat) (mle : Vector f n -> Vector f n -> f) : LookupTable f
+  | ComposedLookupTable (n: Nat) (subtables: Vector (LookupTable f) n) (compose: Vector (LookupTable f) n -> F) : LookupTable f
 
 def lookupTableFromMLE (n: Nat) (mle : Vector f n -> Vector f n -> f) : LookupTable f := LookupTable.LookupTableMLE n mle
 
+def unaryLookupTableFromMLE (n: Nat) (mle : Vector f n -> f) : LookupTable f := LookupTable.LookupTableMLE n mle
+
+-- - Option to define a function for the prover to do witness generation in a more efficient manner
+-- 	- ex: Run xor instead of evaluating the MLE
+-- 
+-- - Indexed vs non-indexed lookups
