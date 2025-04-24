fd01ad267931acde93b0a085676e10a5bc1f9690
James Parker <james@galois.com>
2025-04-24 15:42:29 -0400

    Generic Hoare compiles

--- src/ZkLean/Formalism.lean
+++ src/ZkLean/Formalism.lean
@@ -1,4 +1,6 @@
 
+import Mathlib.Control.Traversable.Basic
+
 import ZkLean.AST
 import ZkLean.Builder
 import ZkLean.LookupTable
@@ -30,7 +32,7 @@ def run_circuit [JoltField f] (circuit: ZKBuilder f a) (state0: ZKBuilderState f
 --   circ_output
 
 -- def eval_expr [JoltField f] [aInst: Encodable a] (expr: aInst.ToZKExpr) (state: ZKBuilderState f) (witness: List f) : a :=
-def eval_expr [JoltField f] (expr: ZKExpr f) (state: ZKBuilderState f) (witness: List f) : Option f :=
+def eval_exprf [JoltField f] (expr: ZKExpr f) (state: ZKBuilderState f) (witness: List f) : Option f :=
   let ram_values := semantics_ram witness state.ram_sizes state.ram_ops
   if let some ram_values := ram_values
   then 
@@ -40,13 +42,16 @@ def eval_expr [JoltField f] (expr: ZKExpr f) (state: ZKBuilderState f) (witness:
   else
     none
 
-def Hoare {f:Type} [JoltField f] [iInst: Encodable i] [oInst: Encodable o] (precondition: i -> Bool) (circuit: iInst.ToZKExpr -> ZKBuilder f oInst.ToZKExpr ) (postcondition: i -> o -> Bool) : Prop :=
+def eval_expr {t: Type -> Type} [Traversable t] [JoltField f] (expr: t (ZKExpr f)) (state: ZKBuilderState f) (witness: List f) : Option (t f) :=
+  traverse (eval_exprf · state witness) expr
+
+def Hoare {f:Type} {i:Type -> Type} {o:Type -> Type} [Traversable i] [Traversable o] [JoltField f] (precondition: i f -> Bool) (circuit: i (ZKExpr f) -> ZKBuilder f (o (ZKExpr f))) (postcondition: i f -> o f -> Bool) : Prop :=
   ∀ state0 : ZKBuilderState f, 
   ∀ witnesses : List f,
-  ∀ input : i,
-  ∀ output : o,
-  ∀ input_expr : iInst.ToZKExpr,
-  ∀ output_expr : oInst.ToZKExpr,
+  ∀ input : i f,
+  ∀ output : o f,
+  ∀ input_expr : i (ZKExpr f),
+  ∀ output_expr : o (ZKExpr f),
   -- ∀ x y : f,
   -- ∀ x_wit y_wit : WitnessId,
   -- ∀ _: x_wit < witnesses.length,
@@ -56,9 +61,9 @@ def Hoare {f:Type} [JoltField f] [iInst: Encodable i] [oInst: Encodable o] (prec
   -- -- witnesses.get (((x_wit: WitnessId): ℕ): Fin witnesses.length) = x →
   -- witnesses[x_wit] = x →
   -- witnesses[y_wit] = y →
-  eval_expr input_expr state0 witness = some input ->
+  eval_expr input_expr state0 witnesses = some input ->
   let statef := (StateT.run (circuit input_expr) state0).2
-  eval_expr output_expr statef witness = some output ->
+  eval_expr output_expr statef witnesses = some output ->
 
   precondition input →
     run_circuit (circuit input_expr) state0 witnesses ↔ postcondition input output -- x_expr y_expr
